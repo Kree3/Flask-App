bfaf712d912b03f742594ff86186e144246ca14f
joohyunl <joohyunl@umich.edu>
2019-01-08 20:31:12 -0500

    navbar links

---
+++ myflaskapp/data.py
@@ -0,0 +1,28 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title': 'Article One',
+			'body': 'Hello World',
+			'author': 'Kevin Lee',
+			'create_date': '01-07-2019'
+		},
+
+		{
+			'id': 2,
+			'title': 'Article Two',
+			'body': 'Hello World',
+			'author': 'Kevin Lee',
+			'create_date': '01-07-2019'
+		},
+
+		{
+			'id': 3,
+			'title': 'Article Three',
+			'body': 'Hello World',
+			'author': 'Kevin Lee',
+			'create_date': '01-07-2019'
+		}
+
+	]
+	return articles
